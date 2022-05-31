129aa485c5f5287b45f03e8ad9c50c9d5abb273a
makarovan <nadezdamakarovaut@gmail.com>
2022-05-31 17:58:29 +0300

    admin modelUsers table userrole instead of roles

--- adminpanel/modelAdmin/ModelUsers.php
+++ adminpanel/modelAdmin/ModelUsers.php
@@ -1,33 +1,33 @@
 <?php
 class ModelUsers{
-	public static function getUsersActionList(){
-	$sql = "SELECT users.*, roles.`roleName` FROM  users,roles WHERE `users`.role = `roles`.idRole  ORDER BY `users`.`idUser` DESC";
-	//создать экземпляр класса database
-	$database = new database();
-	//$rows = массив данных
-	$rows = $database->getAll($sql);
-	return $rows;
-	}
+    public static function getUsersActionList(){
+    $sql = "SELECT users.*, userrole.`rolename` FROM  users,userrole WHERE `users`.role = `userrole`.idRole  ORDER BY `users`.`idUser` DESC";
+    //создать экземпляр класса database
+    $database = new database();
+    //$rows = массив данных
+    $rows = $database->getAll($sql);
+    return $rows;
+    }
 
-	public static function getUserDetail($id){
-		$sql = "SELECT users.* FROM users WHERE `users`.`idUser` = $id";
-		$database = new database();
-		$row = $database->getOne($sql);
-		return $row;
-	}
+    public static function getUserDetail($id){
+        $sql = "SELECT users.* FROM users WHERE `users`.`idUser` = $id";
+        $database = new database();
+        $row = $database->getOne($sql);
+        return $row;
+    }
 
-	public static function editUser($id){
-		$result = false;
-		if (isset($_POST['save'])) {
-			if (isset($_POST['userDescription'])) {
-				$description=$_POST['userDescription'];
-				$sql = "UPDATE `users` SET  `userDescription` = '$description' WHERE `users`.`idUser` = '$id'";
-				$database = new database();
-				$item = $database->executeRun($sql);
-				$result = true;
-			}// isset post name
-		}//isset post save
-	return $result;
-	} //fucn
+    public static function editUser($id){
+        $result = false;
+        if (isset($_POST['save'])) {
+            if (isset($_POST['userDescription'])) {
+                $description=$_POST['userDescription'];
+                $sql = "UPDATE `users` SET  `userDescription` = '$description' WHERE `users`.`idUser` = '$id'";
+                $database = new database();
+                $item = $database->executeRun($sql);
+                $result = true;
+            }// isset post name
+        }//isset post save
+    return $result;
+    } //fucn
 }
 ?>
--- adminpanel/viewAdmin/categoryActionList.php
+++ adminpanel/viewAdmin/categoryActionList.php
@@ -2,43 +2,43 @@
 ob_start();
 ?>
 <h4 class="box-title">Список категорий</h4>
-	<button class="btn btn-success btn-sm ">
-		<a href='addcategory' style='color: white'>Добавить категорию</a>
-	</button>
-	<table class="table">
-		<thead>
-			<tr class="d-flex">
-				<th style="width:30%">id</th>
-				<th style="width:60%">Категория</th>			
-				<th style="width:10%">Action</th>
-			</tr>
-		</thead>
-		<tbody>
-			<?php
-				foreach ($rows as $row) {
-			?>
-			<tr>
-				<td><?php echo $row['idCategory'];?></td>
-				<td><?php echo $row['categoryName'];?></td>		
-				<td>
-					<button class="btn btn-warning btn-sm btn-block">
-						<a href='editcategory?id=<?php echo $row['idCategory'];?>' style='color: white'>Изменить <span class="glyphicon glyphicon-edit"></span></a>
-					</button>							
-					<button class="btn btn-danger btn-sm btn-block">
-						<a href='deletecategory?id=<?php echo $row['idCategory'];?>' style='color: white'>Удалить <span class="glyphicon glyphicon-trash"></span></a>
-					</button>
-				</td>
-			</tr>   
-			<?php
-				}//end foreach
-			?>               
-		</tbody>
-		<tfoot>
-			<tr>
-				<td colspan=3></td>
-			</tr>
-		</tfoot>
-	</table>
+    <button class="btn btn-success btn-sm ">
+        <a href='addcategory' style='color: white'>Добавить категорию</a>
+    </button>
+    <table class="table">
+        <thead>
+            <tr class="d-flex">
+                <th style="width:30%">id</th>
+                <th style="width:60%">Категория</th>			
+                <th style="width:10%">Action</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+                foreach ($rows as $row) {
+            ?>
+            <tr>
+                <td><?php echo $row['idCategory'];?></td>
+                <td><?php echo $row['categoryName'];?></td>		
+                <td>
+                    <button class="btn btn-warning btn-sm btn-block">
+                        <a href='editcategory?id=<?php echo $row['idCategory'];?>' style='color: white'>Изменить <span class="glyphicon glyphicon-edit"></span></a>
+                    </button>							
+                    <button class="btn btn-danger btn-sm btn-block">
+                        <a href='deletecategory?id=<?php echo $row['idCategory'];?>' style='color: white'>Удалить <span class="glyphicon glyphicon-trash"></span></a>
+                    </button>
+                </td>
+            </tr>   
+            <?php
+                }//end foreach
+            ?>               
+        </tbody>
+        <tfoot>
+            <tr>
+                <td colspan=3></td>
+            </tr>
+        </tfoot>
+    </table>
 <?php
-	$content = ob_get_clean();
-	include 'viewAdmin/templates/layout.php';
+    $content = ob_get_clean();
+    include 'viewAdmin/templates/layout.php';
